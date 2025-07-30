9327ae6743ce0d4a78aff840c2ff7e36366313ba
Ha Quanq <quangha0120010@gmail.com>
2025-07-30 08:48:25 +0700

    Update styles

---
+++ src/main.js
@@ -0,0 +1,23 @@
+const menuButton = document.querySelector(".nav__menu-btn");
+const menuWrapper = document.querySelector(".nav__list");
+
+let isMenuOpened = () => menuButton.getAttribute("aria-expanded") === "true";
+
+const showMenu = () => {
+    menuButton.setAttribute("aria-expanded", "true");
+    document.body.style.overflow = "hidden";
+};
+
+const hideMenu = () => {
+    menuButton.setAttribute("aria-expanded", "false");
+    document.body.style.overflow = null;
+};
+
+menuButton.addEventListener("click", () => {
+    isMenuOpened() ? hideMenu() : showMenu();
+});
+
+window.addEventListener("resize", (e) => {
+    const mediaQuery = window.matchMedia("(min-width: 48em)");
+    if (mediaQuery.matches && isMenuOpened()) hideMenu();
+});
